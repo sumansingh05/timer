6bcd18ae635b0503c1d4cae2998744665d436010
sumansingh05 <kumarsumansingh7870@gmail.com>
2024-07-03 18:48:07 +0530

    change applied

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+const reset = document.querySelector(".reset");
+
 document.addEventListener("DOMContentLoaded",function(){
     const timerElement = document.getElementById("timer");
     let seconds = 0;
     let minutes = 0;
     let hours = 0;
+    let intervalId = null;
+
     function pad(number){
         return (number < 10) ? '0' + number : number;
     }
@@ -18,5 +24,28 @@ document.addEventListener("DOMContentLoaded",function(){
         }
         timerElement.textContent = pad(hours) + ':' + pad(minutes) + ':' +  pad(seconds);
     }
-    setInterval(updateTimer,1000);
+    start.addEventListener("click",()=>{
+        if(intervalId === null){
+            intervalId = setInterval(updateTimer,1000);
+        }
+    });
+
+    stop.addEventListener("click",()=>{
+        if(intervalId !== null){
+            clearInterval(intervalId);
+            intervalId = null;
+        }
+    });
+
+    reset.addEventListener("click",()=>{
+        if(intervalId !== null){
+            clearInterval(intervalId);
+            intervalId = null;
+        }
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        timerElement.textContent = "00:00:00";
+    });
+
 });
